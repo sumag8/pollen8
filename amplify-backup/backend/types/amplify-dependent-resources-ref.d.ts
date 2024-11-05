05b518b2da082196e4e9f597c758e1c60e646fd4
sumag8 <pranavig789@gmail.com>
2024-11-05 14:39:07 -0500

    Add files via upload

---
+++ amplify-backup/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "allergyAID": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "allergyAID": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3allergyaidstorageec8205cb": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
